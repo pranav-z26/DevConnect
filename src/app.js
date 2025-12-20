cb43e8fb4bdf7fb17a270b2c5f595d937b93e303
pranav-z26 <pranavzagade2600@gmail.com>
2025-12-20 14:22:20 +0530

    S2 E04 Routing and request handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 7777;
 
-app.use('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.get('/test', (req, res)=> {
+  console.log(req.query); //http://localhost:7777/test?id=101&name=pranav
+  res.send('This is a get test endpoint!'); 
+})
+
+app.get('/test/:id', (req, res)=> {
+  console.log(req.params);
+  const id = req.params.id;
+  res.send(`This is a get test endpoint with id: ${id}`); 
+})
+
+// app.post('/test', (req, res)=> {
+//   res.send({firstName:"Pranav", lastName: "Zagade"}); 
+// })
+
+// app.put('/test', (req, res)=> { 
+//   res.send('Data updated in db!'); 
+// })
+
+// app.delete('/test', (req, res)=> {
+//   res.send('Data deleted from db!'); 
+// })
+
+// app.use('/test', (req, res) => {
+//   res.send('test test test!');
+// });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
